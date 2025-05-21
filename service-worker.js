cbda795a5b3e528dd5cb3128fad1d7c354f3c799
Valira08 <valerio@arrighini.de>
2025-05-21 13:04:44 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,6 @@ self.addEventListener('install', function(e) {
     })
   );
 });
-
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
